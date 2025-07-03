35dfa4a7ddc242a82c2f08330988e737542dc717
JSh4w <jontyshaw@btinternet.com>
2025-07-03 19:28:51 +0000

    Feat: temp addition of test url

--- backend/python-service/start-dev.sh
+++ backend/python-service/start-dev.sh
@@ -4,5 +4,6 @@
 echo "Starting development server..."
 # Add any pre-startup tasks here (database migrations, etc.)
 
+echo "http://localhost:5000/apple"
 # Start server with hot reloading
-uvicorn app.main:app --host 0.0.0.0 --port 5000 --reload
+uvicorn app.main_test:app --host 0.0.0.0 --port 5000 --reload --reload-delay 0.2
