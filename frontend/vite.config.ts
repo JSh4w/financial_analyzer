1d34d07ea2284880abcc837a2274f8108abd5bf6
jonathan <jontyshaw@btinternet.com>
2025-12-04 00:20:10 +0000

    Updated config for running servers from containers

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-
 import path from 'path'
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react()],
-  resolve:{
-    alias:{
-      react:path.resolve(__dirname, './node_modules/react'),
+  server: {
+    host: true,  // or '0.0.0.0' - allows devcontainer port forwarding
+    port: 5173,
+    watch: {
+      usePolling: true  // enables hot reload in containers
+    }
+  },
+  resolve: {
+    alias: {
+      react: path.resolve(__dirname, './node_modules/react'),
       'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
     },
   },
