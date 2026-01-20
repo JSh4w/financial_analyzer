b3521b2b49b52515766b08d51b230f010be0956d
jontysh4w <jontyshaw@btinternet.com>
2026-01-20 19:49:34 +0000

    innit HMM header file

---
+++ backend/stock-service/app/cpp/hmm/src/hmm.h
@@ -0,0 +1,57 @@
+#ifndef HMM_HPP
+#define HMM_HPP
+
+#include <vector>
+#include <array>
+#include <string>
+
+
+/**
+    Hidden Markov Model with Student-t emissions for regime detection on returns
+*/
+class HMM {
+public:
+    // No Parameter as fixed type
+    HMM();
+
+    // Training, to adjust parameter values 
+    void fit();
+
+    // Inference
+    std::vector<int> decode(const std::vector<double>& observations ) const;
+    double log_likelihood(const std::vector<double>& observations ) const;
+
+    // Getters
+    const auto& means() const { return mu_; }
+    const auto& scales() const { return sigma_; }
+    const auto& transitions() const { return A_; }
+
+    // Persistance
+    // save function for post trained parameters
+    void save(const std::string& path) const; // method wwont modify any class members
+    // load function of trained parameters
+    void load(const std::string& path);
+
+private:
+    // Internal state
+    // number of states - we are fixing this for the class
+    // since its a custom HMM not general
+    static constexpr int N = 3;
+    // State probabilities init
+    // used once i.e pi_ -> A[t0]pi_ ...
+    std::array<double , N> pi_;
+    // State Transition Matrix
+    std::array<std::array<double, N>,N> A_;
+    // Emission paramters 
+    // mean, variance and degrees of freedom (might be fixed)
+    std::array<double, N> mu_, sigma_, nu_;
+
+    // Inference helpers, 
+    // Feeding observations of log returns = std::log(prices[t]/ prices[t-1])
+    // returns are scale invariant 
+    std::vector<std::array<double, N>> forward(const std::vector<double>& obs) const;
+    std::vector<std::array<double, N>> backward(const std::vector<doubel>& obs) const;
+    double emission(double x, int state) const;
+
+
+};
